1a359949a6862797aca5ce92092dedeee9305741
daniecon <daniel.contreras@math.ethz.ch>
2022-03-17 01:37:37 +0100

    Added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import Header from './_header'
 import Footer from './_footer'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>NFT Marketplace</title>
+    </Head>
     <body className="min-h-screen bg-gradient-to-tr from-dark-purple-900 via-dark-purple-700 to-dark-purple-900">
       <Header />
       <div className="mt-4 mb-10 mx-4 md:mx-6 lg:mx-20">
@@ -11,6 +16,7 @@ function MyApp({ Component, pageProps }) {
       </div>
       <Footer />
     </body>
+    </>
   )
 }
 
