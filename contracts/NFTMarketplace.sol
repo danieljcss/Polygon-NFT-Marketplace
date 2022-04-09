bb79f3a8de506d50ccbf80e1cb027a376b723c69
daniecon <daniel.contreras@math.ethz.ch>
2022-04-09 21:07:18 +0200

    Changed Token Name

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -33,7 +33,7 @@ contract NFTMarketplace is ERC721URIStorage {
       bool sold
     );
 
-    constructor() ERC721("Metaverse Tokens", "METT") {
+    constructor() ERC721("NFT Polygon", "NFTP") {
       owner = payable(msg.sender);
     }
 
